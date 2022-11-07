0f807c955fa28d72ca0cf28629c4704be18be6d5
Maximilian Moller <maximilian.moller@vintin.de>
2022-11-07 12:14:25 +0100

    -

---
+++ src/app/validators/custom-async.validator.ts
@@ -0,0 +1,22 @@
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {UserServiceService} from '../services/user-service.service';
+
+export class UsernameValidator {
+  static createValidator(userService: UserServiceService): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors> => {
+      return userService
+        .checkIfUsernameExists(control.value)
+        .pipe(
+          map((result: boolean) =>
+            result ? { usernameAlreadyExists: true } : null
+          )
+        );
+    };
+  }
+}
